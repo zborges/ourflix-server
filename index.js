338dd69f92b3ec127a4751580a2e3a111e225122
Zack Borges <zackartborges@gmail.com>
2023-04-30 11:23:16 -0600

    adds server and prettier config

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+console.log(process.env)
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
